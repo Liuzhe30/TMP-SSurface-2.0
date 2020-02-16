fbff1fd573e59ccdfc29af315a43266c0a34465e
刘喆 <32893370+Liuzhe30@users.noreply.github.com>
2020-02-16 22:40:18 +0800

    add files

--- TMP-SSurface-2.0/pre_processing.py
+++ TMP-SSurface-2.0/pre_processing.py
@@ -10,7 +10,7 @@ def format(f, n):
             return f
         else:
             return float(int(f) / (10 ** m) * (10 ** m))
-    return round(f, n - len(str(int(f)))) if len(str(f)) > n+1 else f
+    return round(f, n - len(str(int(f)))) if len(str(f)) > n + 1 else f
 
 dict = {'C':0, 'D':1, 'S':2, 'Q':3, 'K':4,
         'I':5, 'P':6, 'T':7, 'F':8, 'N':9,
@@ -19,28 +19,26 @@ dict = {'C':0, 'D':1, 'S':2, 'Q':3, 'K':4,
         
 class Processor:
     
-    def data_pre_processing(self, fasta, pssm_path, window_length):
-        train_fasta = open(fasta)
-        line = train_fasta.readline()
+    def zpred_pre_processing(self, fasta, pssm_path, window_length):
+        
+        get_fasta = open(fasta)
+        line = get_fasta.readline()
         pdb_id = ""
-        x_train = []
-        y_train = []    
+        x_test = []   
         while line:
             codelist = []
             if(line[0] == ">"):
                 pdb_id = line[1:].strip().split()[0]
-                line = train_fasta.readline()
+                line = get_fasta.readline()
                 continue
-            #print(pdb_id)
-            seq_length = len(line) - 1
+                        
             '''
             #-------- used for feature without one-hot code ----------#
-            
+            #seq_length = len(line) - 1
             t = int((window_length - 1) / 2)
             code = np.zeros(seq_length, int)
             code = np.r_[np.ones(t, int), code]        
             code = np.r_[code, np.ones(t, int)]            
-            
             #-------- used for feature without one-hot code ----------#
             '''
             
@@ -128,21 +126,135 @@ class Processor:
             
             #-------- sliding window (window_length * feature) ---------#
             length = code.shape[0]
-            feature = code.shape[1]
             top = 0
             buttom = window_length
             while(buttom <= length):
-                x_train.append(code[top:buttom])            
+                x_test.append(code[top:buttom])            
                 top += 1
                 buttom += 1
             #print(len(window_list))
             #-------- sliding window (window_length * feature) ---------#
             
-            line = train_fasta.readline()    
+            line = get_fasta.readline()    
             
-        x_train = np.array(x_train)
-        print(x_train.shape)  
-        np.save("temp.npy", x_train)      
+        x_test = np.array(x_test)
+        #print(x_test.shape)  
+        return x_test
+    
+    def data_pre_processing(self, pred_zpred, fasta, pssm_path, window_length):
+        
+        get_fasta = open(fasta)
+        temp = get_fasta.readline()
+        pdb_id = ""
+        index = 0
+        while temp:
+            if(temp[0] == ">"):
+                pdb_id = temp[1:].strip().split()[0]
+                temp = get_fasta.readline()
+                continue
+            for i in temp:
+                if(i != '\n'):
+                    zpred_line = pred_zpred[index][0]
+                    index += 1
+                    w = open("temp/" + pdb_id + ".zpred","a+")
+                    w.write(str(zpred_line) + '\n')
+            temp = get_fasta.readline()
+        
+        get_fasta.seek(0)
+        line = get_fasta.readline()
+        x_test = []   
+        while line:
+            codelist = []
+            if(line[0] == ">"):
+                pdb_id = line[1:].strip().split()[0]
+                line = get_fasta.readline()
+                continue
+            
+            #-------- one-hot encoded (len * 20) ----------#
+            for i in line:
+                if(i != "\n"):
+                    code = dict[i.upper()]
+                    codelist.append(code)
+            data = np.array(codelist)
+            #print('Shape of data (BEFORE encode): %s' % str(data.shape))
+            encoded = to_categorical(data)
+            if(encoded.shape[1] < 20):
+                column = np.zeros([encoded.shape[0], 20 - encoded.shape[1]], int)
+                encoded = np.c_[encoded, column]
+            #print('Shape of data (AFTER  encode): %s\n' % str(encoded.shape))
+            #-------- one-hot encoded (len * 20) ----------#
+            
+            #-------- noseq encoded (len + window_length - 1) * 21 ----------#
+            code = encoded
+            length = code.shape[0]
+            noSeq = np.zeros([length, 1], int)
+            code = np.c_[code, noSeq]
+            
+            t = int((window_length - 1) / 2)
+            
+            code = np.r_[np.c_[np.zeros([t, 20], int), np.ones(t, int)], code]        
+            code = np.r_[code, np.c_[np.zeros([t, 20], int), np.ones(t, int)]]
+            #-------- noseq encoded (len + window_length - 1) * 21 ----------#
+            
+            #-------- add pssm feature ----------# 
+            length = code.shape[0]
+            list_dir = os.getcwd()
+            pssm_path = pssm_path + pdb_id + ".pssm"
+            pssm_file = os.path.join(list_dir, pssm_path)
+            if(os.path.exists(pssm_file)):
+                pssm = open(pssm_file)
+                pssm_matrix = np.zeros([length, 20], int)
+                filelines = pssm.readlines()
+                top = t - 1
+                for pssm_line in filelines:
+                    if(len(pssm_line.split()) == 44):
+                        each_item = pssm_line.split()[2:22]
+                        #print(each_item)
+                        for j in range(0, 20):
+                            if(top == length - 1 - t):
+                                break
+                            try:
+                                pssm_matrix[top, j] = int(each_item[j])
+                            except IndexError:
+                                pass
+                        top += 1
+                code = np.c_[code, pssm_matrix]        
+            else:
+                code = np.c_[code, np.zeros([length, 20], int)]
+            #-------- add pssm feature ----------# 
+            
+            #-------- add zpred feature ----------#
+            length = code.shape[0]
+            zpred = np.zeros([length, 1], float)
+            zpred_file = open("temp/" + pdb_id + ".zpred")
+            zpred_line = zpred_file.readline()
+            i = 0
+            while zpred_line:
+                zpred[i] = float(zpred_line.strip())
+                i += 1
+                zpred_line = zpred_file.readline() 
+            #print(zpred)
+            code = np.c_[code, zpred]        
+            zpred_file.close()
+            os.remove("temp/" + pdb_id + ".zpred")
+            #-------- add zpred feature ----------#
+
+            #-------- sliding window (window_length * feature) ---------#
+            length = code.shape[0]
+            top = 0
+            buttom = window_length
+            while(buttom <= length):
+                x_test.append(code[top:buttom])            
+                top += 1
+                buttom += 1
+            #-------- sliding window (window_length * feature) ---------#
+            
+            line = get_fasta.readline()    
+            
+        x_test = np.array(x_test)
+        #print(x_test.shape)  
+        return x_test  
+               
         
 if __name__=='__main__':
     
@@ -157,5 +269,6 @@ if __name__=='__main__':
     fasta = args.fasta
     pssm_path = args.pssm_path
     
-    processor.data_pre_processing(fasta, pssm_path, window_length) 
+    x_test = processor.zpred_pre_processing(fasta, pssm_path, window_length) 
+    np.save('temp.npy', x_test)
     
---
+++ TMP-SSurface-2.0/run.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import tensorflow as tf
+from keras import layers, models, optimizers
+from keras.utils import to_categorical
+from keras.layers import *
+from keras.models import *
+from keras.optimizers import SGD, Adadelta, Adam
+from keras.callbacks import Callback
+from keras import backend as K
+K.clear_session()
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import argparse
+from keras import callbacks
+from keras import backend as K 
+from pre_processing import Processor
+K.set_image_data_format('channels_last')
+
+def attention_3d_block(inputs):
+    a = Permute((2, 1))(inputs)
+    a = Dense(nb_time_steps, activation='softmax')(a)
+    a_probs = Permute((2, 1), name='attention_vec')(a)
+    output_attention_mul = multiply([inputs, a_probs], name='attention_mul')
+    return output_attention_mul
+
+def cc(y_true, y_pred):
+    x = y_true
+    y = y_pred
+    mx = K.mean(x)
+    my = K.mean(y)
+    xm, ym = x-mx, y-my
+    r_num = K.sum(tf.multiply(xm,ym))
+    r_den = K.sqrt(tf.multiply(K.sum(K.square(xm)), K.sum(K.square(ym))))
+    r = r_num / r_den
+    r = K.maximum(K.minimum(r, 1.0), -1.0)
+    return K.square(r)
+
+def get_lr_metric(optimizer):
+    def lr(y_true, y_pred):
+        return optimizer.lr
+    return lr    
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fasta', default='sample/sample.fasta')
+    parser.add_argument('--pssm_path', default='sample/pssm/')
+    args = parser.parse_args()
+    #print(args)    
+    
+    processor = Processor()
+    fasta = args.fasta
+    pssm_path = args.pssm_path    
+    
+    # get zpred feature
+    adadelta = Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+    lr_metric = get_lr_metric(adadelta)
+    
+    x_zpred = processor.zpred_pre_processing(fasta, pssm_path, 25) 
+    x_zpred = x_zpred.reshape(x_zpred.shape[0], 25, 41, 1)
+    
+    cnn_model_path = 'model/CNN_model.h5'
+    cnn_model = load_model(cnn_model_path, custom_objects={'cc':cc, 'lr':lr_metric})
+    cnn_model.summary()
+    
+    pred_zpred = cnn_model.predict(x_zpred)
+ 
+    #parameters for LSTM
+    window_length = 19
+    rows, cols = window_length, 42
+    nb_lstm_outputs = 700  
+    nb_time_steps = window_length
+    nb_input_vector = cols      
+    
+    # get all features
+    x_test = processor.data_pre_processing(pred_zpred, fasta, pssm_path, window_length) 
+    x_test = x_test.reshape(x_test.shape[0], rows, cols)
+        
+    # LSTM structure
+    inputs = Input(shape=(nb_time_steps, nb_input_vector))
+    drop1 = Dropout(0.01)(inputs)
+    lstm_out = Bidirectional(LSTM(nb_lstm_outputs, return_sequences=True), name='bilstm1')(drop1)
+    lstm_out2 = Bidirectional(LSTM(nb_lstm_outputs, return_sequences=True), name='bilstm2')(lstm_out)
+    attention_mul = attention_3d_block(lstm_out2)
+    attention_flatten = Flatten()(attention_mul)
+    drop2 = Dropout(0.3)(attention_flatten)
+    output = Dense(1, activation='relu')(drop2)
+    model = Model(inputs=inputs, outputs=output)  
+    model.summary()
+
+    # load weights
+    model.load_weights('model/LSTM_weights.h5')
+    y_pred = model.predict(x_test, batch_size=128)
+    
+    print('finished!')
+    
+    # get results
+    get_fasta = open(fasta)
+    temp = get_fasta.readline()
+    pdb_id = ""
+    index = 0
+    while temp:
+        if(temp[0] == ">"):
+            pdb_id = temp[1:].strip().split()[0]
+            temp = get_fasta.readline()
+            continue
+        for i in temp:
+            if(i != '\n'):
+                rASA_line = y_pred[index][0]
+                index += 1
+                w = open("results/" + pdb_id + ".rASA","a+")
+                w.write(str(rASA_line) + '\n')
+        temp = get_fasta.readline()    
+
+    
